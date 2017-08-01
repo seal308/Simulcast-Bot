bc2bc024e9d3108fd6f3b96dd9901c4f93971c08
seal308 <gpj369@gmail.com>
2017-08-01 14:24:41 -0500

    ping pong demo, working on test server

---
+++ src/main/java/simulcastBot/SimulcastBot.java
@@ -0,0 +1,16 @@
+package simulcastBot;
+
+import de.btobastian.javacord.*;
+
+public class SimulcastBot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//DiscordAPI api = Javacord.getApi("MzQwNTA4MzY4MjU3MTU1MDc1.DFzixg.tZCKhfe7Lf8X9uY32OoDpe_DAfE", true);
+		Connecting connect = new Connecting("MzQwNTA4MzY4MjU3MTU1MDc1.DFzixg.tZCKhfe7Lf8X9uY32OoDpe_DAfE");
+		System.out.println("hello");
+	}	
+	
+	
+
+}
